38a580c7fde12e8ece35065975712553c0084daf
ki-hong <rlghd153@gmail.com>
2023-02-09 13:08:40 +0900

    [Add] 프로젝트 생성

---
+++ server/src/main/java/com/hongjo/trailquest/TrailquestApplication.java
@@ -0,0 +1,13 @@
+package com.hongjo.trailquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailquestApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/hongjo/trailquest/TrailquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongjo.trailquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
